7d259dcb41da5cacc217887881a18333211ed40e
DavidMelchor <davidmelchorhachuel@gmail.com>
2021-09-07 11:47:03 +0200

    verano

---
+++ js/main.js
@@ -0,0 +1,230 @@
+$( document ).ready(function() {
+    const extra = 190
+    $("#programar1boton").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 1000);
+    });
+
+    $("#tabla1boton").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#tabla1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#d").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#programar1").offset().top - extra
+      }, 2000);
+    });
+
+    $("#futbolveranoboton").on("click",function() {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#futbolverano").offset().top - extra
+      }, 2000);
+    });
+});
